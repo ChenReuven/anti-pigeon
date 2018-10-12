5021938862c946b5f3af9ec730fd03caf8dd7fdf
reuvec <chen.reuven@dell.com>
2018-10-12 18:26:58 +0300

    1. generate random voice when change detection happens 2. add more audio

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,46 +1,71 @@
 // App Init
-const video = document.getElementById('video');
-const score = document.getElementById('score');
-const body = document.getElementById('body');
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
+const video         = document.getElementById('video');
+const score         = document.getElementById('score');
+const body          = document.getElementById('body');
+const startBtn      = document.getElementById('start-btn');
+const stopBtn       = document.getElementById('stop-btn');
 const fullscreenBtn = document.getElementById('fullscreen-btn');
-let fullscreen = false;
-const status = document.getElementById('status-detection');
-const sound = new Howl({
-	src: ['assets/sound-dog.mp3']
+let fullscreen      = false;
+const status        = document.getElementById('status-detection');
+
+// URLS
+const DOG_SOUND1_URL    = 'assets/audio/dog-sound-1.mp3';
+const DOG_SOUND2_URL    = 'assets/audio/dog-sound-2.wav';
+const BIRD_SOUND1_URL   = 'assets/audio/bird-sound-1.wav';
+const HAWK_SOUND1_URL   = 'assets/audio/hawk-sound-1.mp3';
+const HAWK_SOUND2_URL   = 'assets/audio/hawk-sound-2.wav';
+const HAWK_SOUND3_URL   = 'assets/audio/hawk-sound-3.wav';
+const HAWK_SOUND4_URL   = 'assets/audio/hawk-sound-4.wav';
+const HAWK_SOUND5_URL   = 'assets/audio/hawk-sound-5.mp3';
+const ALDER_SOUND1_URL   = 'assets/audio/alder-sound-1.wav';
+const KRAAIL_SOUND1_URL   = 'assets/audio/kraail-sound-1.wav';
+const antiPigeonsVoices = [
+  DOG_SOUND1_URL,
+  DOG_SOUND2_URL,
+  BIRD_SOUND1_URL,
+  HAWK_SOUND1_URL,
+  HAWK_SOUND2_URL,
+  HAWK_SOUND3_URL,
+  HAWK_SOUND4_URL,
+  HAWK_SOUND5_URL,
+  ALDER_SOUND1_URL,
+  KRAAIL_SOUND1_URL
+];
+
+let sound = new Howl({
+  src: [DOG_SOUND1_URL]
 });
 
 // Slider
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
-let { value: sliderValue } = slider;
-output.innerHTML = sliderValue;
-slider.oninput = function () {
-	output.innerHTML = this.value;
-	sliderValue = this.value;
+const slider             = document.getElementById("myRange");
+const output             = document.getElementById("demo");
+let {value: sliderValue} = slider;
+output.innerHTML         = sliderValue;
+slider.oninput           = function () {
+  output.innerHTML = this.value;
+  sliderValue      = this.value;
 }
 
 // Buttons
-startBtn.addEventListener('click', function() {
-	DiffCamEngine.start();
+startBtn.addEventListener('click', function () {
+  DiffCamEngine.start();
   status.innerText = 'Playing';
 });
 
-stopBtn.addEventListener('click', function() {
-	DiffCamEngine.stop();
+stopBtn.addEventListener('click', function () {
+  DiffCamEngine.stop();
   status.innerText = 'Stopping';
 });
 
-fullscreenBtn.addEventListener('click', function() {
+fullscreenBtn.addEventListener('click', function () {
   fullscreen = !fullscreen;
-  if(fullscreen){
+  if (fullscreen) {
     fullscreenBtn.textContent = 'Exit Full Screen';
     document.documentElement.webkitRequestFullScreen();
-	} else{
+  } else {
     fullscreenBtn.textContent = 'Full Screen';
     document.webkitExitFullscreen();
-	}
+  }
 });
 
 // Diff Cam
@@ -48,38 +73,51 @@ function initSuccess() {
 }
 
 function initError(e) {
-	alert('Something went wrong.');
-	console.log("Init error:" + e);
+  alert('Something went wrong.');
+  console.log("Init error:" + e);
 }
 
 function isSoundPlaying(sound) {
-	return sound.playing();
+  return sound.playing();
 }
 
 function startTime() {
-	body.classList.toggle('red');
+  body.classList.toggle('red');
 }
 
 function flickeringScreen() {
-	const tt = setInterval(function () { startTime() }, 200);
-	setTimeout(function () { clearInterval(tt); body.classList.remove('red') }, 10000);
+  const tt = setInterval(function () {
+    startTime()
+  }, 200);
+  setTimeout(function () {
+    clearInterval(tt);
+    body.classList.remove('red')
+  }, 10000);
 }
 
+let getRandomVoice = function () {
+  return antiPigeonsVoices[Math.floor(Math.random() * antiPigeonsVoices.length)];
+};
+
 function capture(payload) {
-	score.textContent = payload.score;
-	const scoreResult = payload.score;
-	console.log('sliderValue = ', sliderValue);
-	if (scoreResult > sliderValue) {
-		if (!isSoundPlaying(sound)) {
-			sound.play();
-			flickeringScreen();
-		}
-	}
+  score.textContent = payload.score;
+  const scoreResult = payload.score;
+  console.log('sliderValue = ', sliderValue);
+  if (scoreResult > sliderValue) {
+    if (!isSoundPlaying(sound)) {
+      const item = getRandomVoice();
+      sound      = new Howl({
+        src: [item]
+      });
+      sound.play();
+      flickeringScreen();
+    }
+  }
 }
 
 DiffCamEngine.init({
-	video: video,
-	initSuccessCallback: initSuccess,
-	initErrorCallback: initError,
-	captureCallback: capture
+  video              : video,
+  initSuccessCallback: initSuccess,
+  initErrorCallback  : initError,
+  captureCallback    : capture
 });
